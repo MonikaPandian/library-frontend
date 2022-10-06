c3b450e7b557de353e826386e1a245f51e182448
MonikaPandian <panmonikmm@gmail.com>
2022-10-06 15:52:49 +0530

    library-frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Auth from './Auth';
+import Dashboard from './components/Dashboard';
+import Ebooks from './components/Ebooks';
+import { AppProvider } from './context';
+import AddEbooks from './components/AddEbooks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/e-books" element={<Ebooks />} />
+          <Route path="/add-ebooks" element={<AddEbooks />} />
+        </Routes>
+      </AppProvider>
     </div>
   );
 }
